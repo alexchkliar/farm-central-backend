f048b0dc9dcabeaae53c88afde3d131edf10288b
Alexander Chkliar <alex.chkliar@gmail.com>
2022-01-21 01:20:08 -0500

    added page to post new food

--- models/food.js
+++ models/food.js
@@ -6,23 +6,13 @@ const Schema = mongoose.Schema;
 const FoodSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   units: { type: String, required: true, maxLength: 100 },
-  category: { type: String, maxLength: 100 },
+  category: { type: String, required: true, maxLength: 100 },
   location: { type: String, maxLength: 100 },
-  quantity: { type: Number, min: 0, max: 999 },
+  quantity: { type: Number, required: true, min: 0, max: 999 },
   seller: { type: Schema.Types.ObjectId, ref: 'User' },
   price: { type: Number, min: 0, required: true },
   photo: { type: String },
-  rating: { type: Number, min: 0, max: 5 }
+  rating: { type: Number, min: 0, max: 5, default: 5 } // change default later...
 });
 
-// Virtual for this food instance URL.
-FoodSchema.virtual('url').get(function() {
-  return '/catalog/food/' + this._id;
-});
-
-// FoodSchema.virtual('birthDate_yyyy_mm_dd').get(function() {
-//   return DateTime.fromJSDate(this.birthDate).toISODate(); //format 'YYYY-MM-DD'
-// });
-
-// Export model.
 module.exports = mongoose.model('Food', FoodSchema);
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express");
 router = express.Router();
 food_controller = require("../controllers/foodController");
+const Food = require('../models/food')
 
 // router.get("/", food_controller.foodController)
 router.get("/", food_controller.food_list)
+
+router.post("/new", (req, res) => {
+  // console.log(req.body)
+  // console.log(req.user)
+  const newFood = new Food({
+    name: req.body.name,
+    units: req.body.units,
+    category: req.body.category,
+    location: req.body.location,
+    quantity: req.body.quantity,
+    price: req.body.price,
+    photo: req.body.photo,
+    seller: req.user,
+  });
+  newFood.save();
+  res.send("New food saved");
+});
+
 
 module.exports = router;
