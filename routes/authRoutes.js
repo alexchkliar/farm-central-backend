5191ae64476e87d6865f6625eb0218d5e5c15a2d
Alexander Chkliar <alex.chkliar@gmail.com>
2022-01-28 21:33:26 -0500

    adding log to test logout in production 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -30,8 +30,9 @@ router.get("/logout", (req, res) => {
   req.logout();
   console.log("let's see here")
   console.log(REDIRECT_URL)
-  res.send("inside here " + REDIRECT_URL);
-  // res.redirect(REDIRECT_URL);
+  console.log(development)
+  // res.send("inside here " + REDIRECT_URL);
+  res.redirect(REDIRECT_URL);
 });
 
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
