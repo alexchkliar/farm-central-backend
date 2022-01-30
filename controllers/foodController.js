8e3754d4bbad366809266459189bc5e830e87607
Alexander Chkliar <alex.chkliar@gmail.com>
2022-01-30 16:04:37 -0500

    added update listing feature

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,5 +1,17 @@
 const Food = require('../models/food')
 
+exports.selected_food = function (req, res, next) {
+  console.log(req.params)
+  Food.findOne({_id: req.params.id })
+    // .sort([['species', 'ascending']])
+    .exec(function (err, food) {
+      if (err) { return next(err); }
+      // Successful, so render.
+      // console.log(list_foods)
+      res.json({food});
+    })
+};
+
 exports.food_list = function (req, res, next) {
   Food.find()
     // .sort([['species', 'ascending']])
